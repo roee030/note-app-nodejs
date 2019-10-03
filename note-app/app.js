12961540bfb9585188554d7fb0779a270acf73a3
roee030 <roee.angel93@gmail.com>
2019-10-03 15:52:44 +0300

    Update app.js
    4 commands add,remove,showlist and read

--- note-app/app.js
+++ note-app/app.js
@@ -1,9 +1,30 @@
 const yargs = require('yargs')
 
+//Create add command
 yargs.command({
     command: 'add',
     describe: 'add a new note',
     handler: function(){ console.log('using add command')}
 })
+//Create remove command
 
-console.log(yargs.argv);
+yargs.command({
+    command: 'remove',
+    describe: 'remove note',
+    handler: function(){ console.log('using remove command')}
+})
+
+//Create read command
+
+yargs.command({
+    command: 'read',
+    describe: 'read note',
+    handler: function(){ console.log('using read command')}
+})
+yargs.command({
+    command: 'list',
+    describe: 'show list',
+    handler: function(){ console.log('using list command')}
+})
+
+//console.log(yargs.argv)
